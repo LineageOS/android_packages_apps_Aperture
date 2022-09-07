2e6d7c9bd88bfcd134e2db37fe3abf8b3f1be9e3
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-09-07 14:17:33 +0200

    Aperture: Avoid clicking stuff behind previews
    Change-Id: I1a88b13c9636f9c390ec9ed6fb13df9981315db4

--- app/src/main/java/org/lineageos/aperture/CameraActivity.kt
+++ app/src/main/java/org/lineageos/aperture/CameraActivity.kt
@@ -1507,6 +1507,8 @@ open class CameraActivity : AppCompatActivity() {
         }
     }
 
+    fun preventClicks(@Suppress("UNUSED_PARAMETER") view: View) {}
+
     companion object {
         private const val LOG_TAG = "Aperture"
 
