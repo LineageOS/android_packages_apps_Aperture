4a94251362e3299b94bed39f53efc39db044ee0d
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-10-10 02:08:34 +0200

    Aperture: Parse some sensor infos
    Co-authored-by: Vibhor Srivastava <69432426+vibhorSrv@users.noreply.github.com>
    
    Change-Id: I9f05b2c9f9d1bfd0deef16aa77bd535bbb52ad1e

--- app/src/main/java/org/lineageos/aperture/utils/Camera.kt
+++ app/src/main/java/org/lineageos/aperture/utils/Camera.kt
@@ -39,6 +39,18 @@ class Camera(cameraInfo: CameraInfo, cameraManager: CameraManager) {
         camera2CameraInfo.physicalCameraIds.filter { it.isNotEmpty() }.map { it.toInt() }
     val isLogical = physicalCameraIds.isNotEmpty()
 
+    val focalLengths = camera2CameraInfo.getCameraCharacteristic(
+        CameraCharacteristics.LENS_INFO_AVAILABLE_FOCAL_LENGTHS
+    )!!
+    val apertures = camera2CameraInfo.getCameraCharacteristic(
+        CameraCharacteristics.LENS_INFO_AVAILABLE_APERTURES
+    )!!
+    val sensorSize = camera2CameraInfo.getCameraCharacteristic(
+        CameraCharacteristics.SENSOR_INFO_PHYSICAL_SIZE
+    )!!
+
+    val mm35FocalLength = (36.0f / sensorSize.width) * focalLengths[0]
+
     val supportedVideoQualities: MutableList<Quality> =
         QualitySelector.getSupportedQualities(cameraInfo)
 
