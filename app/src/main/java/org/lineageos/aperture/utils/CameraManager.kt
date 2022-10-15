bc87c90c5dca2caf4a36e4d5a90692cd78fd3731
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-10-16 01:47:17 +0200

    Aperture: Handle logical cameras on front/back cameras list
    * Also parse zoom values
    
    Change-Id: Ia1393004cd085e5546905363ef40ccc980e55207

--- app/src/main/java/org/lineageos/aperture/utils/CameraManager.kt
+++ app/src/main/java/org/lineageos/aperture/utils/CameraManager.kt
@@ -32,14 +32,10 @@ class CameraManager(activity: AppCompatActivity) {
         }
 
     // We expect device cameras to never change
-    val backCameras = cameras.values.filter {
-        it.cameraFacing == CameraFacing.BACK
-    }
+    val backCameras = prepareDeviceCamerasList(CameraFacing.BACK)
     val mainBackCamera = backCameras.firstOrNull()
 
-    val frontCameras = cameras.values.filter {
-        it.cameraFacing == CameraFacing.FRONT
-    }
+    val frontCameras = prepareDeviceCamerasList(CameraFacing.FRONT)
     val mainFrontCamera = frontCameras.firstOrNull()
 
     val externalCameras: List<Camera>
@@ -90,5 +86,33 @@ class CameraManager(activity: AppCompatActivity) {
 
     fun shutdown() {
         cameraExecutor.shutdown()
+    }
+
+    private fun prepareDeviceCamerasList(cameraFacing: CameraFacing): List<Camera> {
+        val facingCameras = cameras.values.filter {
+            it.cameraFacing == cameraFacing
+        }
+
+        if (facingCameras.isEmpty()) {
+            return listOf()
+        }
+
+        val mainCamera = facingCameras.first()
+        if (mainCamera.isLogical) {
+            // If first camera is logical, it's very likely that it merges all sensors and handles
+            // them with zoom (e.g. Pixels). Just expose only that
+            return listOf(mainCamera)
+        }
+
+        // Get rid of logical cameras, we want single sensor cameras for now
+        val physicalCameras = facingCameras.filter { !it.isLogical }
+
+        // Setup zoom ratio for aux cameras
+        val auxCameras = physicalCameras.drop(1)
+        for (camera in auxCameras) {
+            camera.zoomRatio = camera.mm35FocalLengths[0] / mainCamera.mm35FocalLengths[0]
+        }
+
+        return physicalCameras
     }
 }
