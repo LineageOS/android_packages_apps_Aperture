d8e81426514e93e7fa216744036796af2e32f3ab
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-10-31 01:45:31 +0100

    Aperture: Add scene mode button
    Change-Id: Ib12b7d480f19e09a6b6f2d18cad23a245121a867

--- app/src/main/java/org/lineageos/aperture/CameraControlExt.kt
+++ app/src/main/java/org/lineageos/aperture/CameraControlExt.kt
@@ -11,6 +11,7 @@ import androidx.camera.camera2.interop.Camera2CameraControl
 import androidx.camera.camera2.interop.CaptureRequestOptions
 import androidx.camera.core.CameraControl
 import com.google.common.util.concurrent.ListenableFuture
+import org.lineageos.aperture.utils.SceneMode
 
 val CameraControl?.camera2CameraControl: Camera2CameraControl?
     @androidx.camera.camera2.interop.ExperimentalCamera2Interop
@@ -28,3 +29,15 @@ fun <T : Any> CameraControl?.setCaptureRequestOption(
         .setCaptureRequestOption(key, value)
         .build()
 )
+
+var CameraControl?.sceneMode: SceneMode
+    @androidx.camera.camera2.interop.ExperimentalCamera2Interop
+    get() = SceneMode.fromValue(
+        getCaptureRequestOption(
+            CaptureRequest.CONTROL_SCENE_MODE
+        ) ?: CaptureRequest.CONTROL_SCENE_MODE_DISABLED
+    ) ?: SceneMode.DISABLED
+    @androidx.camera.camera2.interop.ExperimentalCamera2Interop
+    set(value) {
+        setCaptureRequestOption(CaptureRequest.CONTROL_SCENE_MODE, value.value)
+    }
