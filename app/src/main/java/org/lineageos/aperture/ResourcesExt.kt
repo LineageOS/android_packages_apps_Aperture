ed44b0c29fce41f2c85eb23b69e7112975ed99c7
LuK1337 <priv.luk@gmail.com>
2022-11-05 20:37:16 +0100

    Aperture: Support loading RRO resources for .dev build
    Change-Id: Ice64ed82d7a478c9bb982d35ec349c7e05c13b64

---
+++ app/src/main/java/org/lineageos/aperture/ResourcesExt.kt
@@ -0,0 +1,39 @@
+/*
+ * SPDX-FileCopyrightText: 2022 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.lineageos.aperture
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.res.Resources
+
+const val rroPackageName = "org.lineageos.aperture.auto_generated_rro_vendor__"
+
+private fun getRroResources(context: Context): Resources {
+    return context.packageManager.getResourcesForApplication(rroPackageName)
+}
+
+@SuppressLint("DiscouragedApi")
+private fun Resources.getRroIdentifier(context: Context, id: Int): Int {
+    return getRroResources(context).getIdentifier(
+        getResourceEntryName(id),
+        getResourceTypeName(id),
+        rroPackageName
+    )
+}
+
+internal fun Resources.getBoolean(context: Context, id: Int): Boolean {
+    runCatching {
+        return getRroResources(context).getBoolean(getRroIdentifier(context, id))
+    }
+    return getBoolean(id)
+}
+
+internal fun Resources.getStringArray(context: Context, id: Int): Array<String> {
+    runCatching {
+        return getRroResources(context).getStringArray(getRroIdentifier(context, id))
+    }
+    return getStringArray(id)
+}
