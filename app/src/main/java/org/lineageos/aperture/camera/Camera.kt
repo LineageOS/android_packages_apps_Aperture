24d250fa56bccfeb5fee3baaf149bea137bb2096
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-12-24 19:11:25 +0100

    Aperture: Split ZSL toggle from photo capture mode
    * Also disable vendor extensions if enabled
    
    Change-Id: I72c2242595dee8ed986f8e38d3f27eff5174e6b7

--- app/src/main/java/org/lineageos/aperture/camera/Camera.kt
+++ app/src/main/java/org/lineageos/aperture/camera/Camera.kt
@@ -22,6 +22,7 @@ import kotlin.reflect.safeCast
  */
 @androidx.camera.camera2.interop.ExperimentalCamera2Interop
 @androidx.camera.core.ExperimentalLensFacing
+@androidx.camera.core.ExperimentalZeroShutterLag
 class Camera(cameraInfo: CameraInfo, cameraManager: CameraManager) {
     val cameraSelector = cameraInfo.cameraSelector
 
@@ -82,6 +83,8 @@ class Camera(cameraInfo: CameraInfo, cameraManager: CameraManager) {
             add(VideoStabilizationMode.ON_PREVIEW)
         }
     }.toList()
+
+    val supportsZsl = cameraInfo.isZslSupported
 
     override fun equals(other: Any?): Boolean {
         val camera = this::class.safeCast(other) ?: return false
