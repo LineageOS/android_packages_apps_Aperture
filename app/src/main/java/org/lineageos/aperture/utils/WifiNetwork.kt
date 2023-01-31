ba99e4d5761323a4ff5b81cf1c2e641911430742
Sebastiano Barezzi <seba@sebaubuntu.dev>
2023-01-31 19:31:29 +0100

    Aperture: Add Wi-Fi QR handling
    Change-Id: Ia14955b5b27cf152052591b0fccecee7bebcc28a

---
+++ app/src/main/java/org/lineageos/aperture/utils/WifiNetwork.kt
@@ -0,0 +1,70 @@
+/*
+ * SPDX-FileCopyrightText: 2023 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.lineageos.aperture.utils
+
+import android.net.wifi.WifiNetworkSuggestion
+import android.os.Build
+import androidx.annotation.RequiresApi
+
+data class WifiNetwork(
+    val ssid: String,
+    val isSsidHidden: Boolean = false,
+    val password: String? = null,
+    val encryptionType: EncryptionType = EncryptionType.NONE
+) {
+    enum class EncryptionType {
+        NONE,
+        WEP,
+        WPA;
+    }
+
+    @RequiresApi(Build.VERSION_CODES.Q)
+    fun toNetworkSuggestion(): WifiNetworkSuggestion? = when {
+        encryptionType == EncryptionType.WEP -> null
+        else -> WifiNetworkSuggestion.Builder()
+            .setSsid(ssid)
+            .setIsHiddenSsid(isSsidHidden)
+            .apply {
+                password?.let {
+                    // Wi-Fi QR codes uses "WPA" for WPA/WPA2/WPA3 networks,
+                    // let's pray for the best
+                    setWpa2Passphrase(it)
+                }
+            }
+            .build()
+    }
+
+    companion object {
+        fun fromQr(text: String): WifiNetwork? {
+            val prefix = "WIFI:"
+
+            if (!text.uppercase().startsWith(prefix)) {
+                return null
+            }
+
+            val data = text.removeRange(0, prefix.length).split(";").mapNotNull {
+                runCatching {
+                    with(it.split(":", limit = 2)) {
+                        this[0] to this[1]
+                    }
+                }.getOrNull()
+            }.toMap()
+
+            val ssid = data["S"]?.replace("\\", "") ?: return null
+            val isSsidHidden = data["H"] == "true"
+            val password = data["P"]?.replace("\\", "")
+            val encryptionType = when (data["T"]) {
+                "WEP" -> EncryptionType.WEP
+                "WPA" -> EncryptionType.WPA
+                else -> EncryptionType.NONE
+            }
+
+            return WifiNetwork(
+                ssid, isSsidHidden, password, encryptionType
+            )
+        }
+    }
+}
