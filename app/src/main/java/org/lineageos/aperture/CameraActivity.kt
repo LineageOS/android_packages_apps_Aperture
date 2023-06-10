4961550f40a952393f82286258ed63d11ea8c676
Sebastiano Barezzi <seba@sebaubuntu.dev>
2023-06-10 13:39:25 +0200

    Aperture: Use the view model for views' rotation
    Change-Id: I97f5895e182b7d63dc44b2b7cd24ede933d8bf0d

--- app/src/main/java/org/lineageos/aperture/CameraActivity.kt
+++ app/src/main/java/org/lineageos/aperture/CameraActivity.kt
@@ -510,6 +510,8 @@ open class CameraActivity : AppCompatActivity() {
         initialCameraFacing = sharedPreferences.lastCameraFacing
 
         // Pass the view model to the views
+        capturePreviewLayout.cameraViewModel = cameraViewModel
+        countDownView.cameraViewModel = cameraViewModel
         secondaryBarLayout.cameraViewModel = cameraViewModel
 
         // Restore settings from shared preferences
@@ -1752,12 +1754,6 @@ open class CameraActivity : AppCompatActivity() {
         // Rotate sliders
         exposureLevel.screenRotation = screenRotation
         zoomLevel.screenRotation = screenRotation
-
-        // Rotate countdown
-        countDownView.screenRotation = screenRotation
-
-        // Rotate capture preview buttons
-        capturePreviewLayout.screenRotation = screenRotation
 
         // Rotate primary bar buttons
         galleryButtonCardView.smoothRotate(compensationValue)
