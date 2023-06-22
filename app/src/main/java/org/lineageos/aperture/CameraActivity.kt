9a63325e63037f1dc85e8c92227f4ff2dfe0b696
Sebastiano Barezzi <seba@sebaubuntu.dev>
2023-06-23 00:42:59 +0200

    Aperture: Set UI components visibility on LiveData observer
    Change-Id: I4c9bb5bc4fa0955e3c116f34b4f13419e8fffd74

--- app/src/main/java/org/lineageos/aperture/CameraActivity.kt
+++ app/src/main/java/org/lineageos/aperture/CameraActivity.kt
@@ -955,6 +955,25 @@ open class CameraActivity : AppCompatActivity() {
                     }
                 }.start()
             }
+
+            // Setup UI depending on camera mode
+            when (cameraMode) {
+                CameraMode.QR -> {
+                    secondaryBottomBarLayout.isVisible = false
+                    primaryBarLayoutGroupPhoto.isVisible = false
+                    googleLensButton.isVisible = isGoogleLensAvailable
+                }
+                CameraMode.PHOTO -> {
+                    secondaryBottomBarLayout.isVisible = true
+                    primaryBarLayoutGroupPhoto.isVisible = true
+                    googleLensButton.isVisible = false
+                }
+                CameraMode.VIDEO -> {
+                    secondaryBottomBarLayout.isVisible = true
+                    primaryBarLayoutGroupPhoto.isVisible = true
+                    googleLensButton.isVisible = false
+                }
+            }
         }
 
         // Observe single capture mode
@@ -1626,25 +1645,6 @@ open class CameraActivity : AppCompatActivity() {
             )
         } else {
             camera.cameraSelector
-        }
-
-        // Setup UI depending on camera mode
-        when (cameraMode) {
-            CameraMode.QR -> {
-                secondaryBottomBarLayout.isVisible = false
-                primaryBarLayoutGroupPhoto.isVisible = false
-                googleLensButton.isVisible = isGoogleLensAvailable
-            }
-            CameraMode.PHOTO -> {
-                secondaryBottomBarLayout.isVisible = true
-                primaryBarLayoutGroupPhoto.isVisible = true
-                googleLensButton.isVisible = false
-            }
-            CameraMode.VIDEO -> {
-                secondaryBottomBarLayout.isVisible = true
-                primaryBarLayoutGroupPhoto.isVisible = true
-                googleLensButton.isVisible = false
-            }
         }
 
         // Bind use cases to camera
