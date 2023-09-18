9ce8daffcf57f02123cf7a8930aceadb510deb89
LuK1337 <priv.luk@gmail.com>
2023-09-18 09:33:32 +0200

    Aperture: Hook up gerrit-vote action
    Change-Id: I7f71a6cc8119bb7bad098f55df087009ef87c5dd

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+// yikes Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "7.4.2" apply false
     id("com.android.library") version "7.4.2" apply false
