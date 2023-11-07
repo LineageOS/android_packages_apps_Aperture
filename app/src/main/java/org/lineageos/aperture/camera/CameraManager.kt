c3b566575c8a8dba8ee6e5402401af41cd3a04bf
Alexander Koskovich <akoskovich@pm.me>
2023-11-07 12:28:50 -0500

    Aperture: Don't start app if no cameras are found on the device
    Change-Id: Ia9bce63c36592fc3d602749f1e27ab941ebe0e93

--- app/src/main/java/org/lineageos/aperture/camera/CameraManager.kt
+++ app/src/main/java/org/lineageos/aperture/camera/CameraManager.kt
@@ -108,7 +108,7 @@ class CameraManager(context: Context) {
         context.resources.getBoolean(context, R.bool.config_enableHighResolution)
     }
 
-    private val cameras: List<Camera>
+    public val cameras: List<Camera>
         get() = cameraProvider.availableCameraInfos.map {
             Camera(it, this)
         }.sortedBy { it.cameraId }
