262ef2e0b89cb0cd346f21e755c1bf411bb92656
LuK1337 <priv.luk@gmail.com>
2024-04-18 11:38:50 +0200

    Aperture: Update CameraX to 1.4.0-alpha05
    Change-Id: I003ea35d909eea2117aff465e0e848eb802a8c0d

--- app/src/main/java/org/lineageos/aperture/ApertureApplication.kt
+++ app/src/main/java/org/lineageos/aperture/ApertureApplication.kt
@@ -1,18 +1,27 @@
 /*
- * SPDX-FileCopyrightText: 2023 The LineageOS Project
+ * SPDX-FileCopyrightText: 2023-2024 The LineageOS Project
  * SPDX-License-Identifier: Apache-2.0
  */
 
 package org.lineageos.aperture
 
+import android.annotation.SuppressLint
 import android.app.Application
+import androidx.camera.camera2.internal.CameraIdUtil
 import com.google.android.material.color.DynamicColors
+import org.lineageos.aperture.ext.getStringArray
 
 class ApertureApplication : Application() {
+    @SuppressLint("RestrictedApi")
     override fun onCreate() {
         super.onCreate()
 
         // Observe dynamic colors changes
         DynamicColors.applyToActivitiesIfAvailable(this)
+
+        // Set backward compatible camera ids
+        CameraIdUtil.setBackwardCompatibleCameraIds(
+            resources.getStringArray(this, R.array.config_backwardCompatibleCameraIds).asList()
+        )
     }
 }
