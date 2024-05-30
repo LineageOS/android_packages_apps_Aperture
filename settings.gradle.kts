6a38cc5a2a8e6e0a403b282cdba1c23f1676102f
LuK1337 <priv.luk@gmail.com>
2024-05-30 10:00:56 +0200

    Aperture: Switch to ZXing-C++
    Change-Id: I97a6af09793feac8a51aca3b849925d85f8ebd9d

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,6 +14,7 @@ dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         maven("https://raw.githubusercontent.com/lineage-next/camerax-aperture/bdc457ba0021c05507c4bec14806c120e132a37f/.m2")
+        maven("https://raw.githubusercontent.com/lineage-next/zxingcpp-aperture/4b83512914db6ca3d0e69b015bb1e7a4a21d205a/.m2")
         google()
         mavenCentral()
     }
