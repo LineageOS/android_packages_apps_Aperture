438749871601473a2f48efd7b3e807fb2575de4b
LuK1337 <priv.luk@gmail.com>
2024-06-14 21:48:25 +0200

    Aperture: Add x86/x86_64 ZXing-C++ JNI libs
    Change-Id: I4a62e02e44861612992961282b9d1b727d524438

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,7 +14,7 @@ dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         maven("https://raw.githubusercontent.com/lineage-next/camerax-aperture/edfd86e9c8d6edfb6643fdea0e6af491a371dc5e/.m2")
-        maven("https://raw.githubusercontent.com/lineage-next/zxingcpp-aperture/6d27bbc39b7b53e7de52054d532a61c2b48fa897/.m2")
+        maven("https://raw.githubusercontent.com/lineage-next/zxingcpp-aperture/7c0350df39a3e10a91d660e0e3b83af86e09f997/.m2")
         google()
         mavenCentral()
     }
