a7f50c11f63eb635aaa21ae7001eb9f3e6f3a916
Tommy Webb <tommy@calyxinstitute.org>
2024-08-14 21:36:06 +0000

    Aperture: Fix front camera intent preview mirroring
    With image capture intents, properly mirror the preview image for a
    front-facing camera photo when the "Mirror front camera" feature is on,
    as is the default. Otherwise, in addition to not being mirrored like
    the live preview, the image preview may appear to be upside down.
    
    Issue: calyxos#2642
    Change-Id: I49b6256750ac821c6eefdd28ed7a4c2ec1c519c9

--- app/src/main/java/org/lineageos/aperture/ui/CapturePreviewLayout.kt
+++ app/src/main/java/org/lineageos/aperture/ui/CapturePreviewLayout.kt
@@ -9,6 +9,7 @@ import android.content.Context
 import android.graphics.BitmapFactory
 import android.net.Uri
 import android.util.AttributeSet
+import android.util.Log
 import android.widget.ImageButton
 import android.widget.ImageView
 import androidx.camera.camera2.interop.ExperimentalCamera2Interop
@@ -116,6 +117,7 @@ class CapturePreviewLayout(context: Context, attrs: AttributeSet?) : ConstraintL
             MediaType.PHOTO -> {
                 if (uri != null) {
                     imageView.rotation = 0f
+                    imageView.scaleX = 1f
                     imageView.setImageURI(uri)
                 } else {
                     val inputStream = photoInputStream!!
@@ -123,8 +125,14 @@ class CapturePreviewLayout(context: Context, attrs: AttributeSet?) : ConstraintL
                     inputStream.mark(Int.MAX_VALUE)
                     val bitmap = BitmapFactory.decodeStream(inputStream)
                     inputStream.reset()
+                    Log.d(LOG_TAG, "Preview transform=$transform screenRotation=$screenRotation")
                     imageView.rotation =
                         transform.rotation.offset.toFloat() - screenRotation.offset
+                    imageView.scaleX = if (transform.mirror) {
+                        -1f
+                    } else {
+                        1f
+                    }
                     imageView.setImageBitmap(bitmap)
                 }
             }
@@ -160,5 +168,9 @@ class CapturePreviewLayout(context: Context, attrs: AttributeSet?) : ConstraintL
 
         cancelButton.smoothRotate(compensationValue)
         confirmButton.smoothRotate(compensationValue)
+    }
+
+    companion object {
+        private const val LOG_TAG = "Aperture"
     }
 }
