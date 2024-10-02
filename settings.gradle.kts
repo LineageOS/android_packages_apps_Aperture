7550eaabf3eb2552a532a678d0b51c641761de42
LuK1337 <priv.luk@gmail.com>
2024-10-02 19:17:29 +0200

    Aperture: Update CameraX to 1.5.0-alpha02
    Change-Id: Ib828b048b2375900a9e3ac67b5df861cba381cbd

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,7 +13,7 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        maven("https://raw.githubusercontent.com/lineage-next/camerax-aperture/d6fe3a7d39ecad777c368fd5f4f981b79fbb18cf/.m2")
+        maven("https://raw.githubusercontent.com/lineage-next/camerax-aperture/d4c48cb0de46da4105488c61cdee0bc0f5496158/.m2")
         maven("https://raw.githubusercontent.com/lineage-next/zxingcpp-aperture/7c0350df39a3e10a91d660e0e3b83af86e09f997/.m2")
         google()
         mavenCentral()
