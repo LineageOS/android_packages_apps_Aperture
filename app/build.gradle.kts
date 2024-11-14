ed37b81cb2a02f644074b010d803468917b7af09
LuK1337 <priv.luk@gmail.com>
2024-11-14 09:21:12 +0100

    Aperture: Sync lib version with Twelve
    Change-Id: I5b1b189f584edb296d1303c8a7fbe3d5a8387469

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,14 +78,14 @@ dependencies {
     // Align versions of all Kotlin components
     implementation(platform("org.jetbrains.kotlin:kotlin-bom:1.9.0"))
 
-    implementation("androidx.core:core-ktx:1.10.1")
-    implementation("androidx.activity:activity-ktx:1.7.2")
-    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.activity:activity-ktx:1.9.2")
+    implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.exifinterface:exifinterface:1.3.7")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.5")
     implementation("androidx.preference:preference:1.2.1")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("com.google.android.material:material:1.12.0")
 
     // CameraX core library using the camera2 implementation
     val cameraxVersion = "1.5.0-alpha03"
