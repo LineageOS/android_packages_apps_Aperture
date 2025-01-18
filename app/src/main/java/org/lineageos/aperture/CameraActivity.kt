79e900c6c864b1eaa93f5c1df785cd5218c2dd24
Luca Stefani <luca.stefani.ge1@gmail.com>
2025-01-18 16:34:47 +0100

    Aperture: Fix kotlin 2 build
    Change-Id: I6c6b0ae72b35632c478a7ec7f9b2a7c5409afabf

--- app/src/main/java/org/lineageos/aperture/CameraActivity.kt
+++ app/src/main/java/org/lineageos/aperture/CameraActivity.kt
@@ -434,7 +434,7 @@ open class CameraActivity : AppCompatActivity(R.layout.activity_camera) {
     @get:RequiresApi(Build.VERSION_CODES.Q)
     private val onThermalStatusChangedListener by lazy {
         OnThermalStatusChangedListener {
-            val showSnackBar = { stringId: @receiver:StringRes Int ->
+            val showSnackBar = { stringId: Int ->
                 Snackbar.make(secondaryBottomBarLayout, stringId, Snackbar.LENGTH_INDEFINITE)
                     .setAnchorView(secondaryBottomBarLayout)
                     .setAction(android.R.string.ok) {
@@ -1533,7 +1533,7 @@ open class CameraActivity : AppCompatActivity(R.layout.activity_camera) {
                 // Log the error
                 Log.e(LOG_TAG, "Error: code: ${it.code}, type: ${it.type}", it.cause)
 
-                val showToast = { stringId: @receiver:StringRes Int ->
+                val showToast = { stringId:Int ->
                     Toast.makeText(this, stringId, Toast.LENGTH_SHORT).show()
                 }
 
