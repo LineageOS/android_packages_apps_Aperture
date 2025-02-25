de8e1a3e409bab46095fddee99be803e876908d1
Sebastiano Barezzi <seba@sebaubuntu.dev>
2025-02-25 21:41:15 +0100

    Aperture: lens_launcher: Bump SDK to 35
    Change-Id: Ia7bc77683ca3ef0cd481f7a089a7ee29c8738205

--- lens_launcher/build.gradle.kts
+++ lens_launcher/build.gradle.kts
@@ -10,12 +10,12 @@ plugins {
 
 android {
     namespace = "com.google.android.apps.googlecamera.fishfood"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.google.android.apps.googlecamera.fishfood"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
     }
