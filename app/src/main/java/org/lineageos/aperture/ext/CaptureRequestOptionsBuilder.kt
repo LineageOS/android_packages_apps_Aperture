25f681c1a5cca44db4e7c3086802e3b7614d6bc9
Sebastiano Barezzi <seba@sebaubuntu.dev>
2025-02-26 01:54:36 +0100

    Aperture: Rework Camera2 CaptureRequest application
    Change-Id: Idf88d1f4221501be911b625e75c5d877e0751907

--- app/src/main/java/org/lineageos/aperture/ext/CaptureRequestOptionsBuilder.kt
+++ app/src/main/java/org/lineageos/aperture/ext/CaptureRequestOptionsBuilder.kt
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: 2022-2023 The LineageOS Project
+ * SPDX-FileCopyrightText: 2022-2025 The LineageOS Project
  * SPDX-License-Identifier: Apache-2.0
  */
 
@@ -8,7 +8,6 @@ package org.lineageos.aperture.ext
 import android.hardware.camera2.CameraMetadata
 import android.hardware.camera2.CaptureRequest
 import android.os.Build
-import androidx.annotation.RequiresApi
 import androidx.camera.camera2.interop.CaptureRequestOptions
 import org.lineageos.aperture.models.ColorCorrectionAberrationMode
 import org.lineageos.aperture.models.DistortionCorrectionMode
@@ -20,21 +19,19 @@ import org.lineageos.aperture.models.ShadingMode
 import org.lineageos.aperture.models.VideoStabilizationMode
 
 @androidx.camera.camera2.interop.ExperimentalCamera2Interop
-fun CaptureRequestOptions.Builder.setFrameRate(frameRate: FrameRate?) {
-    frameRate?.let {
-        setCaptureRequestOption(
-            CaptureRequest.CONTROL_AE_TARGET_FPS_RANGE, it.range
-        )
-    } ?: run {
-        clearCaptureRequestOption(CaptureRequest.CONTROL_AE_TARGET_FPS_RANGE)
-    }
-}
+fun CaptureRequestOptions.Builder.setFrameRate(
+    frameRate: FrameRate?
+): CaptureRequestOptions.Builder = frameRate?.let {
+    setCaptureRequestOption(CaptureRequest.CONTROL_AE_TARGET_FPS_RANGE, it.range)
+} ?: clearCaptureRequestOption(CaptureRequest.CONTROL_AE_TARGET_FPS_RANGE)
 
 @androidx.camera.camera2.interop.ExperimentalCamera2Interop
-fun CaptureRequestOptions.Builder.setVideoStabilizationMode(videoStabilizationMode: VideoStabilizationMode) {
+fun CaptureRequestOptions.Builder.setVideoStabilizationMode(
+    videoStabilizationMode: VideoStabilizationMode?
+): CaptureRequestOptions.Builder = videoStabilizationMode?.let {
     setCaptureRequestOption(
         CaptureRequest.CONTROL_VIDEO_STABILIZATION_MODE,
-        when (videoStabilizationMode) {
+        when (it) {
             VideoStabilizationMode.OFF -> CameraMetadata.CONTROL_VIDEO_STABILIZATION_MODE_OFF
             VideoStabilizationMode.ON -> CameraMetadata.CONTROL_VIDEO_STABILIZATION_MODE_ON
             VideoStabilizationMode.ON_PREVIEW ->
@@ -45,89 +42,98 @@ fun CaptureRequestOptions.Builder.setVideoStabilizationMode(videoStabilizationMo
                 }
         }
     )
-}
+} ?: clearCaptureRequestOption(CaptureRequest.CONTROL_VIDEO_STABILIZATION_MODE)
 
 @androidx.camera.camera2.interop.ExperimentalCamera2Interop
-fun CaptureRequestOptions.Builder.setEdgeMode(edgeMode: EdgeMode) {
+fun CaptureRequestOptions.Builder.setEdgeMode(
+    edgeMode: EdgeMode?
+): CaptureRequestOptions.Builder = edgeMode?.let {
     setCaptureRequestOption(
         CaptureRequest.EDGE_MODE,
-        when (edgeMode) {
+        when (it) {
             EdgeMode.OFF -> CameraMetadata.EDGE_MODE_OFF
             EdgeMode.FAST -> CameraMetadata.EDGE_MODE_FAST
             EdgeMode.HIGH_QUALITY -> CameraMetadata.EDGE_MODE_HIGH_QUALITY
             EdgeMode.ZERO_SHUTTER_LAG -> CameraMetadata.EDGE_MODE_ZERO_SHUTTER_LAG
         }
     )
-}
+} ?: clearCaptureRequestOption(CaptureRequest.EDGE_MODE)
 
 @androidx.camera.camera2.interop.ExperimentalCamera2Interop
-fun CaptureRequestOptions.Builder.setNoiseReductionMode(noiseReductionMode: NoiseReductionMode) {
+fun CaptureRequestOptions.Builder.setNoiseReductionMode(
+    noiseReductionMode: NoiseReductionMode?
+): CaptureRequestOptions.Builder = noiseReductionMode?.let {
     setCaptureRequestOption(
         CaptureRequest.NOISE_REDUCTION_MODE,
-        when (noiseReductionMode) {
+        when (it) {
             NoiseReductionMode.OFF -> CameraMetadata.NOISE_REDUCTION_MODE_OFF
             NoiseReductionMode.FAST -> CameraMetadata.NOISE_REDUCTION_MODE_FAST
             NoiseReductionMode.HIGH_QUALITY -> CameraMetadata.NOISE_REDUCTION_MODE_HIGH_QUALITY
             NoiseReductionMode.MINIMAL -> CameraMetadata.NOISE_REDUCTION_MODE_MINIMAL
-            NoiseReductionMode.ZERO_SHUTTER_LAG ->
-                CameraMetadata.NOISE_REDUCTION_MODE_ZERO_SHUTTER_LAG
+            NoiseReductionMode.ZERO_SHUTTER_LAG -> CameraMetadata.NOISE_REDUCTION_MODE_ZERO_SHUTTER_LAG
         }
     )
-}
+} ?: clearCaptureRequestOption(CaptureRequest.NOISE_REDUCTION_MODE)
 
 @androidx.camera.camera2.interop.ExperimentalCamera2Interop
-fun CaptureRequestOptions.Builder.setShadingMode(shadingMode: ShadingMode) {
+fun CaptureRequestOptions.Builder.setShadingMode(
+    shadingMode: ShadingMode?
+): CaptureRequestOptions.Builder = shadingMode?.let {
     setCaptureRequestOption(
         CaptureRequest.SHADING_MODE,
-        when (shadingMode) {
+        when (it) {
             ShadingMode.OFF -> CameraMetadata.SHADING_MODE_OFF
             ShadingMode.FAST -> CameraMetadata.SHADING_MODE_FAST
             ShadingMode.HIGH_QUALITY -> CameraMetadata.SHADING_MODE_HIGH_QUALITY
         }
     )
-}
+} ?: clearCaptureRequestOption(CaptureRequest.SHADING_MODE)
 
 @androidx.camera.camera2.interop.ExperimentalCamera2Interop
 fun CaptureRequestOptions.Builder.setColorCorrectionAberrationMode(
-    colorCorrectionAberrationMode: ColorCorrectionAberrationMode
-) {
+    colorCorrectionAberrationMode: ColorCorrectionAberrationMode?
+): CaptureRequestOptions.Builder = colorCorrectionAberrationMode?.let {
     setCaptureRequestOption(
         CaptureRequest.COLOR_CORRECTION_ABERRATION_MODE,
-        when (colorCorrectionAberrationMode) {
+        when (it) {
             ColorCorrectionAberrationMode.OFF -> CameraMetadata.COLOR_CORRECTION_ABERRATION_MODE_OFF
-            ColorCorrectionAberrationMode.FAST ->
-                CameraMetadata.COLOR_CORRECTION_ABERRATION_MODE_FAST
+            ColorCorrectionAberrationMode.FAST -> CameraMetadata.COLOR_CORRECTION_ABERRATION_MODE_FAST
 
             ColorCorrectionAberrationMode.HIGH_QUALITY ->
                 CameraMetadata.COLOR_CORRECTION_ABERRATION_MODE_HIGH_QUALITY
         }
     )
-}
+} ?: clearCaptureRequestOption(CaptureRequest.COLOR_CORRECTION_ABERRATION_MODE)
 
-@RequiresApi(Build.VERSION_CODES.P)
 @androidx.camera.camera2.interop.ExperimentalCamera2Interop
 fun CaptureRequestOptions.Builder.setDistortionCorrectionMode(
-    distortionCorrectionMode: DistortionCorrectionMode
-) {
-    setCaptureRequestOption(
-        CaptureRequest.DISTORTION_CORRECTION_MODE,
-        when (distortionCorrectionMode) {
-            DistortionCorrectionMode.OFF -> CameraMetadata.DISTORTION_CORRECTION_MODE_OFF
-            DistortionCorrectionMode.FAST -> CameraMetadata.DISTORTION_CORRECTION_MODE_FAST
-            DistortionCorrectionMode.HIGH_QUALITY ->
-                CameraMetadata.DISTORTION_CORRECTION_MODE_HIGH_QUALITY
-        }
-    )
+    distortionCorrectionMode: DistortionCorrectionMode?
+): CaptureRequestOptions.Builder = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+    distortionCorrectionMode?.let {
+        setCaptureRequestOption(
+            CaptureRequest.DISTORTION_CORRECTION_MODE,
+            when (it) {
+                DistortionCorrectionMode.OFF -> CameraMetadata.DISTORTION_CORRECTION_MODE_OFF
+                DistortionCorrectionMode.FAST -> CameraMetadata.DISTORTION_CORRECTION_MODE_FAST
+                DistortionCorrectionMode.HIGH_QUALITY ->
+                    CameraMetadata.DISTORTION_CORRECTION_MODE_HIGH_QUALITY
+            }
+        )
+    } ?: clearCaptureRequestOption(CaptureRequest.DISTORTION_CORRECTION_MODE)
+} else {
+    this
 }
 
 @androidx.camera.camera2.interop.ExperimentalCamera2Interop
-fun CaptureRequestOptions.Builder.setHotPixel(hotPixelMode: HotPixelMode) {
+fun CaptureRequestOptions.Builder.setHotPixelMode(
+    hotPixelMode: HotPixelMode?
+): CaptureRequestOptions.Builder = hotPixelMode?.let {
     setCaptureRequestOption(
         CaptureRequest.HOT_PIXEL_MODE,
-        when (hotPixelMode) {
+        when (it) {
             HotPixelMode.OFF -> CameraMetadata.HOT_PIXEL_MODE_OFF
             HotPixelMode.FAST -> CameraMetadata.HOT_PIXEL_MODE_FAST
             HotPixelMode.HIGH_QUALITY -> CameraMetadata.HOT_PIXEL_MODE_HIGH_QUALITY
         }
     )
-}
+} ?: clearCaptureRequestOption(CaptureRequest.HOT_PIXEL_MODE)
