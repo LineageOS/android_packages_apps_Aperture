f170f33fc297f530cebf41dc5fe58e90f9018473
Luca Stefani <luca.stefani.ge1@gmail.com>
2025-09-21 10:44:24 +0200

    Aperture: Update SDK to 36
    Change-Id: Iaa89b0dd8b9628cba8c0d9f2e70541af540b5891

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,12 +15,12 @@ plugins {
 
 android {
     namespace = "org.lineageos.aperture"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "org.lineageos.aperture"
         minSdk = 26
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
     }
