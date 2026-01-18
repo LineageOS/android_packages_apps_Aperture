d9740917a072f78cb819cbfb16bb0a016c4d6a94
Michael Bestas <mkbestas@lineageos.org>
2026-01-18 03:45:18 +0200

    Aperture: Drop LineageOS copyright years
    Change-Id: I44318fba029052bc817d349360aec43e51b32f65

--- app/src/main/java/org/lineageos/aperture/ui/views/CountDownView.kt
+++ app/src/main/java/org/lineageos/aperture/ui/views/CountDownView.kt
@@ -1,6 +1,6 @@
 /*
  * SPDX-FileCopyrightText: 2014 The Android Open Source Project
- * SPDX-FileCopyrightText: 2022-2025 The LineageOS Project
+ * SPDX-FileCopyrightText: The LineageOS Project
  * SPDX-License-Identifier: Apache-2.0
  */
 
