f0b8e1d7d075247736d6b9097bbabcbe338aaa2b
LuK1337 <priv.luk@gmail.com>
2026-02-11 19:50:02 +0100

    Aperture: Update CameraX to 1.6.0-beta02
    Change-Id: I79e1e6e32275703cd192983dcc54c10bc6a2e591

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: 2022-2025 The LineageOS Project
+ * SPDX-FileCopyrightText: 2022-2026 The LineageOS Project
  * SPDX-License-Identifier: Apache-2.0
  */
 
@@ -72,10 +72,7 @@ dependencies {
     implementation(libs.androidx.camera.lifecycle)
     implementation(libs.androidx.camera.video)
     implementation(libs.androidx.camera.view)
-    implementation(libs.androidx.camera.viewfinder.core) {
-        // Invisible in AOSP, we get it via kotlinx.coroutines anyway
-        exclude("org.jetbrains.kotlinx", "atomicfu")
-    }
+    implementation(libs.androidx.camera.viewfinder.core)
     implementation(libs.androidx.camera.extensions)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.core.ktx)
@@ -96,6 +93,8 @@ dependencies {
 configure<GenerateBpPluginExtension> {
     targetSdk.set(android.defaultConfig.targetSdk!!)
     minSdk.set(android.defaultConfig.minSdk!!)
+    versionCode.set(android.defaultConfig.versionCode!!)
+    versionName.set(android.defaultConfig.versionName!!)
     availableInAOSP.set { module: Module ->
         when {
             module.group.startsWith("androidx") -> {
@@ -104,10 +103,15 @@ configure<GenerateBpPluginExtension> {
                         !module.group.startsWith("androidx.media3")
             }
 
-            module.group.startsWith("org.jetbrains") -> true
+            module.group.startsWith("org.jetbrains") -> {
+                // We provide our own atomicfu
+                !module.name.startsWith("atomicfu")
+            }
+
             module.group == "com.google.android.material" -> true
             module.group == "com.google.auto.value" -> true
             module.group == "com.google.guava" -> true
+            module.group == "javax.inject" -> true
             else -> false
         }
     }
