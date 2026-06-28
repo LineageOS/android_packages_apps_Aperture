b678a0fc8670d7c922cb84396005eddb1186b3ef
LuK1337 <priv.luk@gmail.com>
2026-06-28 10:07:54 +0200

    Aperture: Force dark navigation bar in CameraActivity
    Change-Id: I7edc4dadbd1866db3c06ca03c3081dd7aa4cf203

--- app/src/main/java/org/lineageos/aperture/CameraActivity.kt
+++ app/src/main/java/org/lineageos/aperture/CameraActivity.kt
@@ -57,6 +57,7 @@ import androidx.cardview.widget.CardView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
 import androidx.core.view.children
@@ -373,6 +374,10 @@ open class CameraActivity : AppCompatActivity(R.layout.activity_camera) {
             WindowInsetsControllerCompat.BEHAVIOR_DEFAULT,
             statusBars = false,
         )
+
+        // Force dark navigation bar
+        WindowCompat.getInsetsController(window, window.decorView)
+            .isAppearanceLightNavigationBars = false
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1
             && keyguardManager.isKeyguardLocked
